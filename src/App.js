c7aecbeaf5ba04c636a3d1a827efafc76f6a787e
Sai Nikhileswar <sainikhileswar9@gmail.com>
2023-10-14 21:38:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Task4 from "./Components/Task4";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./Components/task4.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Task4 />;
 }
 
 export default App;
